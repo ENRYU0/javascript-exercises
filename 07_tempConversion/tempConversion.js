c97362715af5c48736741036ddc250622803c1d9
Abdellatif Elfarouah <abdellatiflfr2000@gmail.com>
2025-04-28 13:45:14 +0100

    completing temConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+const convertToCelsius = function(i) {
+    return round((i-32)/1.8 , 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(j) {
+  return round((j*1.8)+32 , 1);
 };
 
 // Do not edit below this line
